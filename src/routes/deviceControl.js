fc144732b318c3fc97dfba2ea18a79b30e86f8e5
Diego Sepúlveda <diegosepu.soto@gmail.com>
2020-02-05 16:35:07 -0300

    Routes fixed

--- src/routes/deviceControl.js
+++ src/routes/deviceControl.js
@@ -17,7 +17,7 @@ const {protectedRoute} = require('../middlewares/authService');
 router.route('/deviceControl/registry').get(getRegistry);
 router.route('/deviceControl/devices').get(getDevices);
 router.route('/deviceControl/device/:id/state-history').get(protectedRoute, getDeviceState);
-router.route('/deviceControl/device/:id/state-config').get(protectedRoute, getDeviceConfig);
+router.route('/deviceControl/device/:id/config-history').get(protectedRoute, getDeviceConfig);
 router.route('/deviceControl/device/:id/config').put(protectedRoute, upadateDeviceConfig);
 router.route('/deviceControl/device/:id/user').get(protectedRoute, getUserByDevice);
 router.route('/deviceControl/device/:id/state').get(protectedRoute, getDeviceLastState);
